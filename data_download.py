cb7904052565b4f50e8efad66a7962509df9dec3
guschang2013 <77599534+guschang2013@users.noreply.github.com>
2023-05-05 00:40:58 +0000

    fix validation for output path

--- data_download.py
+++ data_download.py
@@ -1,7 +1,11 @@
+import os
 import requests
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 output_path = "./data/iris.csv"
+
+if not os.path.exists(os.path.dirname(output_path)):
+    os.makedirs(os.path.dirname(output_path))
 
 response = requests.get(url)
 response.raise_for_status()

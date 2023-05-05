53fe254e8d622b2ee980caa1a47772d66838f8a2
guschang2013 <77599534+guschang2013@users.noreply.github.com>
2023-05-05 00:38:11 +0000

    fix output_path

--- data_download.py
+++ data_download.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-output_path = "data/iris.csv"
+output_path = "./data/iris.csv"
 
 response = requests.get(url)
 response.raise_for_status()

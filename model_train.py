b0a04da07e12f02ffa6f7d53a7d98fc04e08b87c
guschang2013 <77599534+guschang2013@users.noreply.github.com>
2023-05-05 02:10:42 +0000

    debugged

--- model_train.py
+++ model_train.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+import os
+import pickle
 
 features_path = "features/iris_features.csv"
 target_path = "data/iris.csv"
@@ -9,12 +12,17 @@ output_path = "models/iris_model.pkl"
 features = pd.read_csv(features_path)
 target = pd.read_csv(target_path, header=None)[4]
 
+le = LabelEncoder()
+target = le.fit_transform(target)
+
 # Train the model
 model = xgb.XGBClassifier()
 model.fit(features, target)
 
 if not os.path.exists(os.path.dirname(output_path)):
     os.makedirs(os.path.dirname(output_path))
-    
+
 # Save the model
-xgb.save_model(output_path, model)
+#xgb.save_model(output_path, model)
+with open(output_path, 'wb') as f:
+    pickle.dump(model, f)
